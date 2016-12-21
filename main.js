0fd553f3ef80b90817145942aa596aa90a317d44
Marcus Baptiste <marbap45@gmail.com>
2016-12-20 22:22:49 -0400

    Create basic file structure

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+  A-B-C of Javascript
+  
+  Main Script File
+
+*/
+
+var content = document.getElementById("content");
+
+content.innerHTML = "A-B-C of Javascript.<br>Script file working";
+
+
